285750e0e428dc34958fd784d69024df97a50cd6
Asebi <asrk01250250@gmail.com>
2025-05-13 00:05:05 +0900

    delete a few userscript header comments

--- scrapbox-table-of-contents-preview.user.js
+++ scrapbox-table-of-contents-preview.user.js
@@ -8,10 +8,6 @@
 // @grant        GM_addStyle
 // @run-at       document-idle
 // @license      MIT License
-// @homepageURL  https://github.com/[YOUR_USERNAME]/scrapbox-link-preview
-// @supportURL   https://github.com/[YOUR_USERNAME]/scrapbox-link-preview/issues
-// @downloadURL  https://github.com/[YOUR_USERNAME]/scrapbox-link-preview/raw/main/scrapbox-link-preview.user.js
-// @updateURL    https://github.com/[YOUR_USERNAME]/scrapbox-link-preview/raw/main/scrapbox-link-preview.user.js
 // @icon         https://scrapbox.io/favicon.ico
 // ==/UserScript==
 
